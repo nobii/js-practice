852511b4ea974f10174e6c1f59292691b5a26eff
fujisawa-shin <fujisawa-shin@kayac.com>
2016-06-22 14:28:39 +0900

    変数代入つけましょう

--- new/js/script.js
+++ new/js/script.js
@@ -1,9 +1,9 @@
 // 普通、クラスはこうやって使う
-new Clock(document.getElementById('clock1'));
+const clock1 = new Clock(document.getElementById('clock1'));
 
 // ↓これで、左右の時計が同じように動くための、customNewを書いて下さい
 // すなわち、「new」の挙動を自作の関数で再現して下さい
-customNew(Clock, [ document.getElementById('clock2') ]);
+const clock2 = customNew(Clock, [ document.getElementById('clock2') ]);
 
 
 
