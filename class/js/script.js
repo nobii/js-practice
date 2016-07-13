eea9998a9cc6c6617c87620cfcef40a0ace7768e
fujisawa-shin <fujisawa-shin@kayac.com>
2016-07-14 01:13:05 +0900

    classのサンプルできた

--- class/js/script.js
+++ class/js/script.js
@@ -1,17 +1,55 @@
-/* ========================================= *
- * class
- * ========================================= */
+const MIN_INTERVAL = 1000;
+const MAX_INTERVAL = 2000;
 
-class Hoge {
-    constructor (name) {
-        console.log(name);
-    }
+const chara = document.querySelector('.js-chara');
+
+
+let x = 0;
+let y = 0;
+
+let w, h, timer;
+
+function setStageSize () {
+    w = Math.floor(window.innerWidth / chara.offsetWidth);
+    h = Math.floor(window.innerHeight / chara.offsetHeight);
+    x = Math.min(x, w);
+    y = Math.min(y, h);
 }
 
+function walk() {
+    timer = setTimeout(() => {
+        const isVertical = (Math.random() < 0.5);
+        let size = (Math.random() < 0.5) ? 1 : -1;
 
+        if ((isVertical && (y + size < 0 || y + size > h)) || (!isVertical && (x + size < 0 || x + size > w))) {
+            size *= -1;
+        }
 
-/* ========================================= *
- * main
- * ========================================= */
+        let orientation;
+        if (isVertical) {
+            if (size > 0) {
+                orientation = 'down';
+            } else {
+                orientation = 'up';
+            }
+        } else {
+            if (size > 0) {
+                orientation = 'right';
+            } else {
+                orientation = 'left';
+            }
+        }
+        chara.setAttribute('data-orientation', orientation);
+        
+        isVertical ? (y += size) : (x += size);
+        
+        chara.style.left = `${x * chara.offsetWidth}px`;
+        chara.style.top = `${y * chara.offsetHeight}px`;
+        walk();
+    }, MIN_INTERVAL + (MAX_INTERVAL - MIN_INTERVAL) * Math.random());
+}
 
-new Hoge('hoge');
+window.addEventListener('resize', setStageSize);
+
+setStageSize();
+walk();
